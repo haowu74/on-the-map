f77ac989c390ad41d7acf3090d9c15c54f03861a
Hao Wu <hao.wu.au@gmail.com>
2018-03-09 22:18:45 +1100

    created login  view

---
+++ On The Map/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  On The Map
+//
+//  Created by Hao Wu on 9/3/18.
+//  Copyright © 2018 S&J. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - LoginViewController: UIViewController
+
+class LoginViewController: UIViewController {
+    
+}
